89d8c82ecff23ddc6d33252196b516c848cca77c
DarkJoker521 <samuelkoh17@gmail.com>
2023-06-27 17:10:35 +0800

    14/5/2023, retried on 27/6/2023, tried to rename the project to MADPractical

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//instantiates new user
         User myUser = new User();
         myUser.setName("Hello World!");
         myUser.setDescription("Hi this represents a long text. \n" +
